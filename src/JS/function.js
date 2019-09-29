fce51a0434d0d27f13fa25333821da487bb379a6
Margarita Samsonova <ritiksamsonova@yandex.ru>
2019-09-29 09:30:09 +0300

    Updates

--- src/JS/function.js
+++
@@ -1,5 +0,0 @@
-export function createNode(tag, tagClass){
-    const element = document.createElement(tag);
-    element.classList.add(tagClass);
-    return element;
-}
--- src/index.js
+++
@@ -1,3 +0,0 @@
-import "./index.css";
-
-import "./JS/script.js";
